1508c190ff58ec7f3ee72755cb20a10125d7aefe
sergio <sergio.salinas@naranya.com>
2017-09-25 15:03:10 -0500

    menu changes

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -1,14 +1,8 @@
 <header class="main-header">
     <!-- Logo -->
-    <a href="{{ url('/admin/home') }}" class="logo"
-       style="font-size: 16px;">
-        <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini">
-           @lang('quickadmin.quickadmin_title')</span>
-        <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg">
-           @lang('quickadmin.quickadmin_title')</span>
-    </a>
+            <a href="{{ url('/') }}" class="logo">
+                <img class="full-height" src="/uploads/avatars/wba_large.png" alt="{{ env('APP_NAME', 'FaBe Manager') }}">
+            </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
         <!-- Sidebar toggle button-->
