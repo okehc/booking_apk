15e38ec02d761278c5968953f09c752098018666
sergio <sergio.salinas@naranya.com>
2017-09-25 22:59:15 -0500

    last finnaly

--- app/Http/Controllers/Admin/AccesosController.php
+++ app/Http/Controllers/Admin/AccesosController.php
@@ -48,7 +48,7 @@ class AccesosController extends Controller
         }
 
         try {
-            $ubicaciones= DB::connection('mysql')->select('SELECT id, nombre, estado FROM ubicaciones') ;    
+            $ubicaciones= DB::connection('mysql')->raw('SELECT id, nombre, estado FROM ubicaciones') ;    
 
             $ub_array = array();
             foreach($ubicaciones as $ubicacion) {
