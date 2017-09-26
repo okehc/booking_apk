ba8ca318db47eaba0916de48c1a19e31eb995ba0
sergio <sergio.salinas@naranya.com>
2017-09-25 23:05:52 -0500

    last finnaly

--- resources/views/admin/accesos/create.blade.php
+++ resources/views/admin/accesos/create.blade.php
@@ -12,7 +12,7 @@
 
 <?php 
 
-var_dump($ub_array);
+var_dump($ubicaciones);
 
 ?>
 
@@ -20,9 +20,7 @@ var_dump($ub_array);
             <div class="row">
                 <div class="col-xs-12 form-group">
                     {!! Form::label('nombre_acceso', trans('quickadmin.accesos.fields.nombre-acceso').'*', ['class' => 'control-label']) !!}
-                    {!! Form::text('nombre_acceso', old('nombre_acceso'), ['class' => 'form-control', 'placeholder' => 'Nombre del acceso a crear', 'required' => '']) !!}
-
-                    
+                    {!! Form::text('nombre_acceso', old('nombre_acceso'), ['class' => 'form-control', 'placeholder' => 'Nombre del acceso a crear', 'required' => '']) !!}            
                     <p class="help-block">Nombre del acceso a crear</p>
                     @if($errors->has('nombre_acceso'))
                         <p class="help-block">
@@ -34,7 +32,7 @@ var_dump($ub_array);
             <div class="row">
                 <div class="col-xs-12 form-group">
                     {!! Form::label('id_ubicacion', trans('quickadmin.accesos.fields.id-ubicacion').'*', ['class' => 'control-label']) !!}
-                    {!! Form::number('id_ubicacion', old('id_ubicacion'), ['class' => 'form-control', 'placeholder' => 'Ubicación a la que pertenece', 'required' => '']) !!}
+                    {!! Form::select('id_ubicacion', $ubicaciones), ['class' => 'form-control', 'placeholder' => 'Nombre del acceso a crear']) !!}
                     <p class="help-block">Ubicación a la que pertenece</p>
                     @if($errors->has('id_ubicacion'))
                         <p class="help-block">
