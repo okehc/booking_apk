78e44bc4d388b5f01358e2b648406fbf39d44c43
sergio <sergio.salinas@naranya.com>
2017-09-27 17:28:57 -0500

    edit changes

--- app/Http/Controllers/Admin/AccesosController.php
+++ app/Http/Controllers/Admin/AccesosController.php
@@ -34,7 +34,7 @@ class AccesosController extends Controller
             
             try {
                 $accesos= DB::connection('mysql')->select('SELECT a.id, a.nombre_acceso, b.nombre as id_ubicacion, a.created_at, a.updated_at, a.deleted_at 
-                                                           FROM accesos a JOIN ubicaciones b on a.id = b.id ') ;                
+                                                           FROM accesos a JOIN ubicaciones b on a.id_ubicacion = b.id ') ;                
             } catch (\Exception $ubicaciones) {
                 die("Could not connect to the database.  Please check your configuration.");
             }
