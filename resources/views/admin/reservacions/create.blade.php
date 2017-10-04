25664a863c204cb7de0a7afcb64de0cd08d774d7
sergio <sergio.salinas@naranya.com>
2017-10-04 01:38:02 -0500

    timepicker

--- resources/views/admin/reservacions/create.blade.php
+++ resources/views/admin/reservacions/create.blade.php
@@ -2,22 +2,12 @@
 
 @section('javascript')
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    <!-- Jquery -->
-    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-    <!-- Datepicker Files -->
-    <link rel="stylesheet" href="{{asset('datePicker/css/bootstrap-datepicker3.css')}}">
-    <link rel="stylesheet" href="{{asset('datePicker/css/bootstrap-standalone.css')}}">
-    <script src="{{asset('datePicker/js/bootstrap-datepicker.js')}}"></script>
-    <!-- Languaje -->
-    <script src="{{asset('datePicker/locales/bootstrap-datepicker.es.min.js')}}"></script>
- 
+
+  <script type="text/javascript" src="{{asset('picker/moment.js"></script>
+<script type="text/javascript" src="{{asset('picker/transition.js"></script>
+<script type="text/javascript" src="{{asset('picker/collapse.js"></script>
+<script type="text/javascript" src="{{asset('picker/bootstrap.min.js"></script>
+<script type="text/javascript" src="{{asset('picker/bootstrap-datetimepicker.min.js"></script>
 
     <script>
     $(function() {        
@@ -39,16 +29,9 @@
             $('#' + $(this).val()).show();
         });
         
-        $('.datepicker').datepicker({
-            format: "dd/mm/yyyy",
-            language: "es",
-            autoclose: true,
-            daysOfWeekDisabled: "0,6",
-            daysOfWeekHighlighted: "1",
-            calendarWeeks: true, 
-            todayHighlight: true,
-            
-        });
+
+        $('.datepicker').datetimepicker();
+
 
     });
     </script>
