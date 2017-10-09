a414c8e13b629f1f554e0d739bc96832fdbf3854
sergio <sergio.salinas@naranya.com>
2017-10-09 03:57:41 -0500

    add atach file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin/reservacions/index');
+        return view('admin/calendar');
     }
 }
