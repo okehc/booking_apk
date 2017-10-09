fbc98ccc59efb753708e455ec7abd18241131edc
sergio <sergio.salinas@naranya.com>
2017-10-09 01:12:16 -0500

    add atach file

--- app/Http/Controllers/Admin/ReservacionsController.php
+++ app/Http/Controllers/Admin/ReservacionsController.php
@@ -168,7 +168,7 @@ class ReservacionsController extends Controller
 
                         var_dump($finalResult);
 
-                        for ($i=0; $i < count($finalResult); $i++) { 
+                        for ($i=0; $i < count($finalResult); $i=$i+$daysBetween) { 
                             #$reservation = DB::connection('odbc')->insert("INSERT INTO reservaciones (created_at, nombre_reunion, id_ubicacion, id_seccion, fecha_inicio, hora_inicio, tiempo_duracion, message, repeat) VALUES (getdate(), '".$request->nombre_de_reunion."', ".$request->ubicacion.", ".$request->sala_de_juntas.", '".$finalResult[$i]."', '".$h_inicio."', '".$h_duracion."', '".$request->comentario."', ".$repeat.")");
                         }
 
