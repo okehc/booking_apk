884ca8dc3370554cc76dcec266aafa9f283bf3e9
sergio <sergio.salinas@naranya.com>
2017-10-09 17:06:54 -0500

    add atach file

--- app/Http/Controllers/Admin/EventoController.php
+++ app/Http/Controllers/Admin/EventoController.php
@@ -11,13 +11,20 @@ use App\Http\Controllers\Controller;
  
 class EventoController extends Controller
 {
-    public function api()
+    public function index()
     {
-		$reservacions= DB::connection('odbc')->select("SELECT a.id, a.nombre_reunion as title, a.fecha_inicio as hora_duracion, a.hora_inicio as start, a.hora_inicio + a.tiempo_duracion as end FROM reservaciones a WHERE a.id_usuario = ".$userId." ");
- 	
- 		var_dump($reservacions);
 
-        return response()->json($data); //para luego retornarlo y estar listo para consumirlo
+    	$userId = Auth::id();
+		$reservacions= DB::connection('odbc')->select("SELECT a.id, a.nombre_reunion as title, a.fecha_inicio as fecha, a.hora_inicio as time_start, a.tiempo_duracion as time_end FROM reservaciones a WHERE a.id_usuario = ".$userId." ");
+ 	
+		foreach ($reservacions as $key ) {
+			$t[] =  array('title' => $key['title'], 'fecha' => $key['fecha'], 'time_start' => $key['time_start'], 'time_end' => $key['time_start'] + $key['time_end']  );
+
+		}
+
+ 		var_dump($t);
+
+        #return response()->json($data); //para luego retornarlo y estar listo para consumirlo
  
     }
 }
