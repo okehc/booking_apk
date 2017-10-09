51d812c4d1f5369f9cecf3c642946c193194f527
sergio <sergio.salinas@naranya.com>
2017-10-09 17:19:28 -0500

    add atach file

--- app/Http/Controllers/Admin/EventoController.php
+++ app/Http/Controllers/Admin/EventoController.php
@@ -18,7 +18,12 @@ class EventoController extends Controller
 		$reservacions= DB::connection('odbc')->select("SELECT a.id, a.nombre_reunion as title, a.fecha_inicio as fecha, a.hora_inicio as time_start, a.tiempo_duracion as time_end FROM reservaciones a WHERE a.id_usuario = ".$userId." ");
  	
 		foreach ($reservacions as $key ) {
-			$t[] =  array('title' => $key->title, 'fecha' => $key->fecha, 'time_start' => $key->time_start, 'time_end' => $key->time_start + $key->time_end  );
+
+			$h_inicio = date('H:i:s', strtotime($key->time_start));
+        	$h_duracion = date('H:i:s', strtotime($key->time_end));	
+        	$t_final = $h_inicio + $h_duracion;
+			
+			$t[] =  array('title' => $key->title, 'fecha' => $key->fecha, 'time_start' => $key->time_start, 'time_end' => $t_final  );
 
 		}
 
