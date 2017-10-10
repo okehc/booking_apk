d9897af78d826975a06726d36db85ea84a3017ed
sergio <sergio.salinas@naranya.com>
2017-10-10 00:22:30 -0500

    minuta validation

--- resources/views/admin/reservacions/show.blade.php
+++ resources/views/admin/reservacions/show.blade.php
@@ -81,7 +81,7 @@
             </div>
 
             <p>&nbsp;</p>
-            {{ Form::open([ action' => 'Admin\ReservacionsController@sendMinuta']) }}
+            {{ Form::open([ 'action' => 'Admin\ReservacionsController@sendMinuta']) }}
             <div class="box-body">
                 <div class="form-group">
                     {{Form::label('title', 'Minuta')}}
