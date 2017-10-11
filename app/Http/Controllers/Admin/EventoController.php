b292eea61ae67e3a681b922995c827e43e460c37
sergio <sergio.salinas@naranya.com>
2017-10-10 20:24:42 -0500

    minuta validation

--- app/Http/Controllers/Admin/EventoController.php
+++ app/Http/Controllers/Admin/EventoController.php
@@ -15,16 +15,29 @@ class EventoController extends Controller
     {
 
     	$userId = Auth::id();
-		$reservacions= DB::connection('odbc')->select("SELECT a.id, a.nombre_reunion as title, a.fecha_inicio as fecha, a.hora_inicio as time_start, a.tiempo_duracion as time_end FROM reservaciones a WHERE a.id_usuario = ".$userId." ");
+		$reservacions= DB::connection('odbc')->select("SELECT 
+			a.id, 
+			a.nombre_reunion as title, 
+			a.fecha_inicio as fecha, 
+			a.hora_inicio as time_start, 
+			a.tiempo_duracion as time_end, 
+			b.name, 
+			b.apellido_paterno,
+			c.nombre_seccion
+			FROM reservaciones a  
+			JOIN users b ON a.id_usuario = b.id 
+			JOIN  seccions c ON a.id_seccion  = c.id");
  	
 		foreach ($reservacions as $key ) {
+
+			$real_title = $key->nombre_seccion.", ".$key->name.", ".$key->apellido_paterno;
 
 			$secs = strtotime($key->time_end)-strtotime("00:00:00");
 			$end_time = date("H:i:s",strtotime($key->time_start)+$secs);
 			$sTime = date('Y-m-d H:i:s', strtotime("$key->fecha $key->time_start"));
 			$eTime = date('Y-m-d H:i:s', strtotime("$key->fecha $end_time"));
 
-			$t[] =  array("title" => $key->title, "start" => $sTime, "end" => $eTime, "url" => "http://10.30.42.27/booking/public/admin/reservacions/".$key->id." ");
+			$t[] =  array("title" => $real_title, "start" => $sTime, "end" => $eTime, "url" => "http://10.30.42.27/booking/public/admin/reservacions/".$key->id." ");
 
 		}
 		return json_encode($t);
